ce2d927e628c0e64c5aa3b55d37c2f7e7b9b3f75
Yaroslav Volohov <yvolohov@gmail.com>
2017-08-14 05:53:36 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+/* https://gist.github.com/michalochman/d64360541a484e16817c */
+
+const gulp = require('gulp');
+const browser = require('gulp-browser');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+const transforms = [
+  {
+	  transform: 'babelify',
+	  options: {
+	    presets: ['env', 'react', 'babili'],
+      sourceMaps: true
+	  }
+  }
+];
+
+gulp.task('default', function() {
+  return gulp.src('src/index.js')
+    .pipe(sourcemaps.init())
+    .pipe(browser.browserify(transforms))
+    .pipe(rename('app.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dest'));
+});
---
+++ src/rectangle.js
@@ -0,0 +1,12 @@
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  show() {
+    console.log(`Width: ${this.width}; Height: ${this.height};`);
+  }
+}
+
+export default Rectangle;
